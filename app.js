e303b2889d6e99b27744b15fd4153946f1b863ff
obaidullah1209 <mobaidullah554@gmail.com>
2024-10-30 02:21:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var spider  = document.getElementById("spider")
+
+function imageHandler() {
+ spider.src = "./images/spider gif.gif"
+}
+
+function imageOutHandler() {
+      spider.src = "./images/spiderman.webp"
+  }
+
+
+
+  var gocku = document.getElementById("gocku")
+function imageHandler1() {
+    gocku.src = "./images/gocku gifs.gif"
+}
+
+function imageOutHandler1() {
+      gocku.src = "./images/gocku.jpeg"
+  }
+
+
+  var orange = document.getElementById("orange")
+function imageHandler2() {
+    orange.src = "./images/orange gif.gif"
+}
+
+function imageOutHandler2() {
+      orange.src = "./images/orange.png"
+  }
+
+
+  var car= document.getElementById("car")
+function imageHandler3() {
+    car.src = "./images/car.gif"
+}
+
+function imageOutHandler3() {
+      car.src = "./images/car.jpg"
+  }
+
+
+  var flower= document.getElementById("flower")
+  function imageHandler4() {
+      flower.src = "./images/flowergif.gif"
+  }
+  
+  function imageOutHandler4() {
+        flower.src = "./images/flower.webp"
+    }
+
+
+
+    var mosambi= document.getElementById("mosambi")
+  function imageHandler5() {
+      mosambi.src = "./images/mosambigif.jpeg"
+  }
+  
+  function imageOutHandler5() {
+        mosambi.src = "./images/mosambi.jpg"
+    }
